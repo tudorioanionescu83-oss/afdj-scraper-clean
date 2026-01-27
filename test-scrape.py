9c7389756b5377ef17fefaa557174cb1317d86b3
tudorioanionescu83-oss <tudorioanionescu83@gmail.com>
2026-01-27 18:58:18 +0200

    Create test-scrape.py

---
+++ test-scrape.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+print("✅ Test dependințe OK!")
+
+sites = [
+    "https://afdj.ro/ro/cotele-dunarii",
+    "https://www.cotele-dunarii.ro"
+]
+
+for site in sites
